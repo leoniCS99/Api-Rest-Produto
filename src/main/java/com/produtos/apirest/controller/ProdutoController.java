779677c274638a46e1be07a19e7fa586529fd868
leoni cesar <leonecezar99@gmail.com>
2022-05-29 01:11:28 -0300

    Inclusão do Swagger na Aplicaçao

--- src/main/java/com/produtos/apirest/controller/ProdutoController.java
+++ src/main/java/com/produtos/apirest/controller/ProdutoController.java
@@ -2,29 +2,50 @@ package com.produtos.apirest.controller;
 
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value="/api")
+@Api(value="API REST Produtos")
 public class ProdutoController {
 
     @Autowired
     private ProdutoRepository produtoRepository;
 
     @GetMapping("/produtos")
+    @ApiOperation(value = "Retorna uma lista de Produtos")
     public List<Produto> listaProdutos(){
         return produtoRepository.findAll();
     }
     
     @GetMapping("/produto/{id}")
+    @ApiOperation(value = "Retorna um Produto unico")
     public Produto listaProdutoUnico(@PathVariable(value = "id") long id){
         return produtoRepository.findById(id);
+    }
+
+    @PostMapping("/produto")
+    @ApiOperation(value = "Salva um Produto")
+    public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+
+    @DeleteMapping("/produto")
+    @ApiOperation(value = "Remove um Produto")
+    public void removeProduto(@RequestBody Produto produto){
+        produtoRepository.delete(produto);
+    }
+
+    @PutMapping("/produto")
+    @ApiOperation(value = "Atualiza dados Produto")
+    public Produto atualizaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
     }
 
 }
